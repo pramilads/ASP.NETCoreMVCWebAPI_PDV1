32985ddcce9cc1f19c479aa1121f20e2404d76b9
dspra <dspra@DESKTOP-4D44EP2>
2020-10-06 20:18:54 -0500

    Changes to File - Initial

---
+++ ASP.NETCoreMVCWebAPI_PD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NETCoreMVCWebAPI_PD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
